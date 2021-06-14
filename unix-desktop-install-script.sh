709d013ae2650fdc926c072ec98e1c3b106668f0
kkamara <kelvinkamara@protonmail.com>
2021-06-14 12:39:58 +0100

    Add docker to install script

--- unix-desktop-install-script.sh
+++ unix-desktop-install-script.sh
@@ -4,6 +4,17 @@ if [[ "$USER" == "root" ]]; then
     exit 1
 fi
 
+read -p 'Are you using ubuntu os? (Y/n): ' isUbuntuOs
+isUbuntuOs="${isUbuntuOs,,}" # to lowercase string
+
+installingDocker=1
+if [[ "$isUbuntuOs" == "y" ]]; then
+    echo 'We will be installing docker & docker-compose during this script...'
+else
+    installingDocker=0
+    echo 'We will skip installing docker & docker-compose during this script...'
+fi
+
 echo "Updating, upgradng & autoremoving..."
 sudo apt-get update -y &&
     sudo apt-get upgrade -y &&
@@ -47,6 +58,45 @@ wget https://mega.nz/linux/MEGAsync/xUbuntu_16.04/amd64/megasync_4.4.0-1.1_amd64
 
 echo "Installing spotify..."
 sudo snap install spotify
+
+if [[ "$installingDocker" == 1 ]]; then
+    echo "Installing docker..."
+    sudo apt-get purge -y docker \
+        docker-engine \
+        docker.io \
+        containerd \
+        runc
+    
+    sudo apt-get update
+
+    sudo apt-get install -y apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+    
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | 
+        sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    echo \
+        "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    
+    sudo apt-get install -y docker-ce \
+        docker-ce-cli \
+        containerd.io
+    
+    echo "Installing docker-compose..."
+    sudo rm /usr/local/bin/docker-compose 2>&1
+
+    sudo curl -L \
+        "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+        -o /usr/local/bin/docker-compose
+    
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
 
 echo "To manually download..."
 manualInstallTools=(
