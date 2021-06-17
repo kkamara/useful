3cb1accbb232685c1e738bdba7807c296d6aa8a3
kkamara <kelvinkamara@protonmail.com>
2021-06-17 18:02:04 +0100

    Update desktop install script

--- unix-desktop-install-script.sh
+++ unix-desktop-install-script.sh
@@ -1,26 +1,32 @@
+YELLOW="\033[1;33m"
+GREEN="\033[0;32m"
+CYAN="\033[0;36m" 
+LIGHTGRAY="\033[0;37m"
+RED="\033[0;31m"
+NC="\033[0m" # No Color
 
 if [[ "$USER" == "root" ]]; then
-    echo "Please run this script as non-root user"
+    echo -e "${RED}Please run this script as non-root user${NC}"
     exit 1
 fi
 
-read -p 'Are you using ubuntu os? (Y/n): ' isUbuntuOs
+read -p "Are you using ubuntu os? (Y/n): " isUbuntuOs
 isUbuntuOs="${isUbuntuOs,,}" # to lowercase string
 
 installingDocker=1
 if [[ "$isUbuntuOs" == "y" ]]; then
-    echo 'We will be installing docker & docker-compose during this script...'
+    echo -e "${GREEN}We will be installing docker & docker-compose during this script...${NC}"
 else
     installingDocker=0
-    echo 'We will skip installing docker & docker-compose during this script...'
+    echo -e "${GREEN}We will skip installing docker & docker-compose during this script...${NC}"
 fi
 
-echo "Updating, upgradng & autoremoving..."
+echo -e "${GREEN}Updating, upgradng & autoremoving...${NC}"
 sudo apt-get update -y &&
     sudo apt-get upgrade -y &&
     sudo apt-get autoremove -y
 
-echo "Downloading package tools..."
+echo -e "${GREEN}Downloading package tools...${NC}"
 packageTools=(
     wget
     exo-utils
@@ -34,33 +40,65 @@ packageTools=(
     zip
 )
 for packageTool in "${packageTools[@]}"
-    do
-        echo "- Downloading $packageTool"
+    do :
+        echo -e "${GREEN}- Downloading $packageTool${NC}"
         sudo apt-get install -y $packageTool
     done
 
-installPath="$HOME/Downloads"
-echo "Downloading tools to $installPath..."
-echo "Downloading xampp..."
-wget https://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/7.4.20/xampp-linux-x64-7.4.20-0-installer.run?ts=gAAAAABgxTJ9V7E870D28F96cXrVcfydhjfG8QZZaopd4CzJL8eCx_m12mmADkBbg5f0xd51lt6FTZdzbc8v0iOCW_U8h91uwA%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fxampp%2Ffiles%2Flatest%2Fdownload $installPath
-echo "Downloading vscode..."
-wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 $installPath
-echo "Downloading dbeaver..."
-wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb $installPath
-echo "Downloading zoom..."
-wget https://zoom.us/client/latest/zoom_amd64.deb $installPath
-echo "Downloading discord..."
-wget https://discord.com/api/download?platform=linux&format=deb $installPath
-echo "Downloading slack..."
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.16.0-amd64.deb $installPath
-echo "Downloading mega..."
-wget https://mega.nz/linux/MEGAsync/xUbuntu_16.04/amd64/megasync_4.4.0-1.1_amd64.deb $installPath
+dpkgPackages=()
+forParallelProcessing=()
 
-echo "Installing spotify..."
+installPath="$HOME/Downloads"
+echo -e "${GREEN}Downloading tools to $installPath...${NC}"
+
+echo -e "${GREEN}Downloading composer...${NC}"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/bin/composer
+
+echo -e "${GREEN}Downloading xampp...${NC}"
+xamppPath="$installPath/xampp-linux-x64-8.0.7-0-installer.run"
+forParallelProcessing+=("nohup $xamppPath &")
+wget https://www.apachefriends.org/xampp-files/8.0.7/xampp-linux-x64-8.0.7-0-installer.run -P "$installPath"
+chmod +x "$xamppPath"
+
+echo -e "${GREEN}Downloading dbeaver...${NC}"
+dbeaverPath="$installPath/dbeaver-ce_latest_amd64.deb"
+dpkgPackages+=("$dbeaverPath")
+wget "https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb" -P "$installPath"
+#echo -e "${GREEN}Installing dbeaver...${NC}"
+#sudo rm /var/lib/dpkg/lock && sudo rm /var/lib/apt/lists/lock && sudo dpkg -i "$dbeaverPath" && apt install -fy
+
+echo -e "${GREEN}Downloading slack...${NC}"
+slackPath="$installPath/slack-desktop-4.16.0-amd64.deb"
+dpkgPackages+=("$slackPath")
+wget "https://downloads.slack-edge.com/linux_releases/slack-desktop-4.16.0-amd64.deb" -P "$installPath"
+#echo -e "${GREEN}Installing slack...${NC}"
+#sudo dpkg -i "$slackPath" && apt install -fy
+
+echo -e "${GREEN}Downloading zoom...${NC}"
+zoomPath="$installPath/zoom_amd64.deb"
+dpkgPackages+=("$zoomPath")
+wget https://zoom.us/client/latest/zoom_amd64.deb -P "$installPath"
+echo -e "${GREEN}Installing zoom dependencies...${NC}"
+sudo apt-get install -y libxcb-xtest0
+
+echo -e "${GREEN}Downloading mega...${NC}"
+megaPath="$installPath/megasync_4.4.0-1.1_amd64.deb"
+dpkgPackages+=("$megaPath")
+wget "https://mega.nz/linux/MEGAsync/xUbuntu_16.04/amd64/megasync_4.4.0-1.1_amd64.deb" -P "$installPath"
+
+echo -e "${GREEN}Installing spotify...${NC}"
 sudo snap install spotify
 
+echo -e "${GREEN}Installing nvm...${NC}"
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+
 if [[ "$installingDocker" == 1 ]]; then
-    echo "Installing docker..."
+    echo -e "${GREEN}Installing docker...${NC}"
     sudo apt-get purge -y docker \
         docker-engine \
         docker.io \
@@ -78,7 +116,7 @@ if [[ "$installingDocker" == 1 ]]; then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | 
         sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-    echo \
+    echo -e \
         "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
@@ -88,7 +126,7 @@ if [[ "$installingDocker" == 1 ]]; then
         docker-ce-cli \
         containerd.io
     
-    echo "Installing docker-compose..."
+    echo -e "${GREEN}Installing docker-compose...${NC}"
     sudo rm /usr/local/bin/docker-compose 2>&1
 
     sudo curl -L \
@@ -98,14 +136,37 @@ if [[ "$installingDocker" == 1 ]]; then
     sudo chmod +x /usr/local/bin/docker-compose
 fi
 
-echo "To manually download..."
+# print packages requiring manual download
+echo -e "${GREEN}To manually download...${NC}"
 manualInstallTools=(
-    postman
-    chrome
-    composer
+    "chrome https://www.google.com/intl/en_uk/chrome/"
+    "composer https://getcomposer.org/download/"
+    "discord https://discord.com/download"
+    "postman https://www.postman.com/downloads/"
+    "vscode https://code.visualstudio.com/download"
 )
 for manualInstallTool in "${manualInstallTools[@]}"
-    do
-        echo "- $manualInstallTool"
+    do :
+        echo -e "- $manualInstallTool"
     done
+
+# print sudo run message
+echo -e "${GREEN}Run the following as root...${NC}"
+printf "${LIGHTGRAY}\tsudo dpkg -i  \\"
+
+for dpkgPackage in "${dpkgPackages[@]}"
+	do :
+		printf "\n\t\t$dpkgPackage \\"
+	done
+
+printf "\n\t && sudo apt install -fy \\"
+
+printf "\n\t "
+
+for toProcess in "${forParallelProcessing[@]}"
+	do :
+		printf "&& $toProcess"
+	done
+
+printf "\n${NC}"
 
