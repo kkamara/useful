b104bfcad49e9f6a6850bb76434c279909baf979
kkamara <kelvinkamara@protonmail.com>
2021-06-20 15:36:42 +0100

    Add cli base

--- debian-desktop-setup-v2/go.mod
+++ debian-desktop-setup-v2/go.mod
@@ -3,7 +3,8 @@ module github.com/kkamara/useful/debian-desktop-setup
 go 1.16
 
 require (
-	github.com/fatih/color v1.12.0 // indirect
+	github.com/fatih/color v1.12.0
 	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 )
--- debian-desktop-setup-v2/main.go
+++ debian-desktop-setup-v2/main.go
@@ -1,9 +1,60 @@
 package main
 
 import (
-	"github.com/fatih/color"
+	"fmt"
+	_ "github.com/fatih/color"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
+
+var (
+	appName  = "Debian Desktop Setup v2"
+	appUsage = "installs debian desktop tools & packages custom for https://kelvinkamara.com"
 )
 
 func main() {
-	color.Yellow("Unix Desktop Setup v2")
+	var (
+		installPath  string = "$HOME/Downloads"
+		isUbuntuOs   bool
+		skipComposer bool
+	)
+
+	app := &cli.App{
+		Name:  appName,
+		Usage: appUsage,
+		Action: func(c *cli.Context) error {
+			fmt.Println(
+				installPath,
+				isUbuntuOs,
+				skipComposer,
+			)
+			return nil
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "install-path",
+				Value:       "$HOME/Downloads",
+				Usage:       "Dir path for storing downloaded resources",
+				Destination: &installPath,
+			},
+			&cli.BoolFlag{
+				Name:        "is-ubuntu-os",
+				Value:       false,
+				Usage:       "Specify whether your installed os is a Ubuntu distro (Y/n)",
+				Destination: &isUbuntuOs,
+			},
+			&cli.BoolFlag{
+				Name:        "skip-composer",
+				Value:       false,
+				Usage:       "Skips composer setup (Y/n)",
+				Destination: &skipComposer,
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
