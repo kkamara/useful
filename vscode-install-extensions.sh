98c6838ee4be78f4f6e79d2bf9c46ff4b05bdba6
Kel Kamara <31970496+kkamara@users.noreply.github.com>
2023-06-01 15:10:14 +0100

    Update vscode-install-extensions.sh

--- vscode-install-extensions.sh
+++ vscode-install-extensions.sh
@@ -1,29 +1,18 @@
 repos=(
-batisteo.vscode-django
-bibhasdn.django-html
-bibhasdn.django-snippets
-bigonesystems.django
 bmewburn.vscode-intelephense-client
 CoenraadS.bracket-pair-colorizer-2
 deerawan.vscode-faker
-DhavalVira.django-import-libraries
-dsznajder.es7-react-js-snippets
 eamodio.gitlens
-Equinusocio.vsc-community-material-theme
 mblode.twig-language-2
 MehediDracula.php-namespace-resolver
-ms-python.python
-ms-python.vscode-pylance
-ms-toolsai.jupyter
-ms-toolsai.jupyter-keymap
-ms-toolsai.jupyter-renderers
 neilbrayfield.php-docblocker
-nizami.update-python-packages-and-requirements-txt
 onecentlin.laravel-blade
-PKief.material-icon-theme
-shamanu4.django-intellisense
 Tyriar.lorem-ipsum
-victorzevallos.vscode-theme-django
+DEVSENSE.phptools-vscode
+DEVSENSE.profiler-php-vscode
+Meezilla.json
+rangav.vscode-thunder-client
+rodrigovallades.es7-react-js-snippets
 )
 
 for repo in "${repos[@]}"
