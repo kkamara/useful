76ef04d4cc88d43feed91dbbbc57e2513b8a4e13
kkamara <kamaracomputers@gmail.com>
2023-07-08 20:04:42 +0100

    Update vscode-install-extensions.sh

--- vscode-install-extensions.sh
+++ vscode-install-extensions.sh
@@ -1,5 +1,8 @@
 repos=(
+amandeepmittal.pug
+bmewburn.vscode-intelephense-client
 deerawan.vscode-faker
+DEVSENSE.composer-php-vscode
 dzhavat.bracket-pair-toggler
 eamodio.gitlens
 mblode.twig-language-2
@@ -9,6 +12,8 @@ onecentlin.laravel-blade
 rangav.vscode-thunder-client
 rodrigovallades.es7-react-js-snippets
 Tyriar.lorem-ipsum
+vscodevim.vim
+wordpresstoolbox.wordpress-toolbox
 )
 
 for repo in "${repos[@]}"
