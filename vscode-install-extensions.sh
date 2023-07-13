c388f919b3838aabb80c880578b393b994fbc1d4
Kelvin Kamara <31970496+kkamara@users.noreply.github.com>
2023-07-13 18:51:18 +0100

    Update vscode-install-extensions.sh

--- vscode-install-extensions.sh
+++ vscode-install-extensions.sh
@@ -1,26 +1,15 @@
 repos=(
 amandeepmittal.pug
-batisteo.vscode-django
-bmewburn.vscode-intelephense-client
 deerawan.vscode-faker
-donjayamanne.python-environment-manager
-donjayamanne.python-extension-pack
 dzhavat.bracket-pair-toggler
 eamodio.gitlens
-KevinRose.vsc-python-indent
-mblode.twig-language-2
 Meezilla.json
 MehediDracula.php-namespace-resolver
-ms-python.python
-ms-python.vscode-pylance
-njpwerner.autodocstring
+ms-vscode.cpptools
 onecentlin.laravel-blade
 rangav.vscode-thunder-client
 rodrigovallades.es7-react-js-snippets
 Tyriar.lorem-ipsum
-VisualStudioExptTeam.intellicode-api-usage-examples
-VisualStudioExptTeam.vscodeintellicode
-wholroyd.jinja
 )
 
 for repo in "${repos[@]}"
