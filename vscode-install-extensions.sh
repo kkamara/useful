48f6d31e29b884373e83d10715fa75914c60564b
kkamara <kamaracomputers@gmail.com>
2025-04-30 21:23:02 +0100

    Update vscode-install-extensions.sh

--- vscode-install-extensions.sh
+++ vscode-install-extensions.sh
@@ -1,26 +1,14 @@
 repos=(
 csstools.postcss
 deerawan.vscode-faker
-devsense.composer-php-vscode
-devsense.intelli-php-vscode
-devsense.phptools-vscode
-devsense.profiler-php-vscode
-donjayamanne.jquerysnippets
+docker.docker
 dsznajder.es7-react-js-snippets
 dzhavat.bracket-pair-toggler
 eamodio.gitlens
 eriklynd.json-tools
-golang.go
-mehedidracula.php-namespace-resolver
 ms-azuretools.vscode-docker
-ms-python.debugpy
-ms-python.python
-ms-python.vscode-pylance
-onecentlin.laravel-blade
 redhat.vscode-yaml
-rodrigovallades.es7-react-js-snippets
 tyriar.lorem-ipsum
-whatwedo.twig
 )
 
 for repo in "${repos[@]}"
