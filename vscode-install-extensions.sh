5752e38043ba7c2dfa03622e4d5a80a695ae7aee
kkamara <kamaracomputers@gmail.com>
2026-01-30 15:17:11 +0000

    Update vscode-install-extensions.sh

--- vscode-install-extensions.sh
+++ vscode-install-extensions.sh
@@ -6,13 +6,23 @@ dsznajder.es7-react-js-snippets
 dzhavat.bracket-pair-toggler
 eamodio.gitlens
 eriklynd.json-tools
+github.copilot
+github.copilot-chat
+mathiasfrohlich.kotlin
 ms-azuretools.vscode-containers
 ms-azuretools.vscode-docker
 perkovec.emoji
 pkief.material-icon-theme
+redhat.java
 redhat.vscode-yaml
 tyriar.lorem-ipsum
-xabikos.javascriptsnippet
+vscjava.vscode-gradle
+vscjava.vscode-java-debug
+vscjava.vscode-java-dependency
+vscjava.vscode-java-pack
+vscjava.vscode-java-test
+vscjava.vscode-maven
+xabikos.javascriptsnippets
 )
 
 for repo in "${repos[@]}"
