4126ac5f909706e593201d572f4dc8879cf8ba10
kkamara <kamaracomputers@gmail.com>
2026-05-30 14:02:19 +0100

    Cleanup vscode-install-extensions.sh

--- vscode-install-extensions.sh
+++ vscode-install-extensions.sh
@@ -5,12 +5,10 @@ docker.docker
 dsznajder.es7-react-js-snippets
 eamodio.gitlens
 eriklynd.json-tools
-esbenp.prettier-vscode
 ms-azuretools.vscode-containers
 ms-azuretools.vscode-docker
 perkovec.emoji
 pkief.material-icon-theme
-prisma.prisma
 redhat.vscode-yaml
 tyriar.lorem-ipsum
 xabikos.javascriptsnippets
