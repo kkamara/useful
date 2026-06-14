e440d67d40abd95a5684aa2c8d722bbc6516daed
kkamara <kamaracomputers@gmail.com>
2026-06-14 03:32:02 +0100

    Update vscode-install-extensions.sh

--- vscode-install-extensions.sh
+++ vscode-install-extensions.sh
@@ -9,10 +9,15 @@ ms-azuretools.vscode-containers
 ms-azuretools.vscode-docker
 perkovec.emoji
 pkief.material-icon-theme
+redhat.java
 redhat.vscode-yaml
-rust-lang.rust-analyzer
-tamasfe.even-better-toml
 tyriar.lorem-ipsum
+vscjava.vscode-gradle
+vscjava.vscode-java-debug
+vscjava.vscode-java-dependency
+vscjava.vscode-java-pack
+vscjava.vscode-java-test
+vscjava.vscode-maven
 xabikos.javascriptsnippets
 zhuangtongfa.material-theme
 )
